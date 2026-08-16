e4127dead69c08282997ac517416e90db47b8757
KinGMssN <mrmohith001@gmail.com>
2026-08-16 21:42:35 +0530

    Time: 51 ms (56.49%) | Memory: 30.4 MB (88.55%) - LeetSync

--- 2156-stone-game-ix/stone-game-ix.py
+++ 2156-stone-game-ix/stone-game-ix.py
@@ -1,20 +1,18 @@
 class Solution:
     def stoneGameIX(self, stones):
-        a = 0
-        b = 0
-        c = 0
+        cnt = [0, 0, 0]
         for x in stones:
-            if x % 3 == 0:
-                a += 1
-            elif x % 3 == 1:
-                b += 1
-            else:
-                c += 1
-
-        if a % 2 == 0:
-            return b > 0 and c > 0
-            
-        return abs(b - c) > 2
+            cnt[x % 3] += 1
+        
+        c0, c1, c2 = cnt
+        
+        if c1 == 0 and c2 == 0:
+            return False
+        
+        if c0 % 2 == 0:
+            return c1 > 0 and c2 > 0
+        else:
+            return abs(c1 - c2) > 2
         
 
         
