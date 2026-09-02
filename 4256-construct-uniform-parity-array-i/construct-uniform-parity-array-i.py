3163f80763e47e3c2f2d63ed1567d3d390b355e1
KinGMssN <mrmohith001@gmail.com>
2026-09-02 16:49:42 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (47.47%) - LeetSync

---
+++ 4256-construct-uniform-parity-array-i/construct-uniform-parity-array-i.py
@@ -0,0 +1,10 @@
+class Solution:
+    def uniformArray(self, nums1: list[int]) -> bool:
+        #even-even =even,odd-odd = even and |odd-even|=odd
+        #it always return True because 
+        #if all are same its true
+        #if atleat one is diffrent you can make the entire elements in one order
+        return True
+        
+
+        
