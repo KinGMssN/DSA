1fe21617d3e1c3b51f6953ac101c73a0485f012b
KinGMssN <mrmohith001@gmail.com>
2026-09-03 08:58:39 +0530

    Time: 5 ms (99.39%) | Memory: 35.4 MB (51.83%) - LeetSync

--- 4258-construct-uniform-parity-array-ii/construct-uniform-parity-array-ii.py
+++ 4258-construct-uniform-parity-array-ii/construct-uniform-parity-array-ii.py
@@ -1,5 +1,3 @@
 class Solution:
     def uniformArray(self, nums1: list[int]) -> bool:
-        mn=min(nums1)
-        c=sum(1 for x in nums1 if x%2==1)
-        return mn%2 ==1 or c==0
+        return (min(nums1)%2 ==1 or sum(1 for x in nums1 if x%2==1)==0)
